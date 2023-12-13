ec0418e9cceae92fc0c2917302bb077c797735dc
Hahyun Baek <hahyun8587@gmail.com>
2023-12-13 21:19:11 +0900

    Consider invalid move in _expand_and_backup()

--- alphazero/node.py
+++ alphazero/node.py
@@ -160,12 +160,19 @@ class Node():
         
         if self._children[a] == None:
             s_exp = self._simulator.simulate(self._s, a)
-            p_exp, v_exp = self._model(s_exp[np.newaxis, :].astype(np.float64), 
-                                       False)
-            p_exp = p_exp.numpy().reshape(-1)
-            v_exp = v_exp.numpy().astype(int).reshape(-1)
-            self._children[a] = Node(s_exp, v_exp, p_exp,
-                                     self._simulator.is_terminal(s_exp, a))
+            
+            if s_exp is None:
+                v_exp = 1.0
+                self._children[a] = Node(None, v_exp, None, True)
+            elif self._simulator.is_terminal(s_exp, a):
+                v_exp = -1.0
+                self._children[a] = Node(s_exp, v_exp, None, True)
+            else:     
+                p_exp, v_exp = self._model(s_exp[np.newaxis, :].astype(np.float64), 
+                                           False)
+                p_exp = p_exp.numpy().reshape(-1)
+                v_exp = v_exp.numpy().astype(int).reshape(-1)
+                self._children[a] = Node(s_exp, v_exp, p_exp, False)
         else: 
             v_exp = self._children[a]._expand_and_backup()        
         
