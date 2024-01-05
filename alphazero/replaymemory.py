13cfdf4f06055318fb008769f885578dbc67d30e
Hahyun Baek <hahyun8587@gmail.com>
2024-01-05 22:52:54 +0900

    use numpy instead of tensorflow in sample() and fix attribute of ReplayMemory

--- alphazero/replaymemory.py
+++ alphazero/replaymemory.py
@@ -1,39 +1,44 @@
-import tensorflow as tf
+import numpy as np
 from collections import deque
 
 class ReplayMemory(deque):
     """Replay memory that saves data generated from each episode.
     
     Args:
-        thresh (int): The number of episode to be saved.
+        ep_cap (int): The number of episodes can be saved.
     """    
     
     #instance variables
-    _thresh: int
+    _ep_cap: int
     _min_ep: int
+    _rng: np.random.Generator
     
-    def __init__(self, thresh: int):
+    def __init__(self, ep_cap: int):
         super(ReplayMemory, self).__init__()
 
-        self._thresh = thresh
+        self._ep_cap = ep_cap
         self._min_ep = 0
+        self._rng = np.random.default_rng()
     
     
     def extend(self, data: list) -> None:
         """Saves `data` to this instance.
         
-        All the data from least number of episode are discarded if the number of 
-        episodes in this instance exceeds `thresh`.
+        All the data from minimum episode number are discarded 
+            if this instance already has `ep_cap` numbers of episodes.
         
         Args:
-            data (list): The data to be saved. The data should be consists of
-                list of [episode number, s, pi, z], where:
+            data (list): The data to be saved. The data should be a
+                list of episode number, s, pi, and z, where:
             * `s` indicates state
             * `pi` indicates policy from mcts
             * `z` indicates real result value
         """
-    
-        if data[0][0] > self._thresh:
+        
+        if self._min_ep == 0:
+            self._min_ep += 1
+        
+        if data[0][0] > self._min_ep + self._ep_cap - 1:
             while 1:
                 if len(self) == 0:
                     break
@@ -45,7 +50,6 @@ class ReplayMemory(deque):
                     
                     break
             
-            self._thresh += 1
             self._min_ep += 1
         
         super(ReplayMemory, self).extend(data)
@@ -55,23 +59,21 @@ class ReplayMemory(deque):
         """Samples random `n` data from this instance.
 
         Args:
-            n (int): The number of samples
+            n (int): The number of data to be sampled.
         
         Returns:
-            tuple: Sampled data.
+            tuple: The sampled data. The tuple is consist of 
+                x and list of pi and z.
         """
 
         x = []
         y_pi = []
         y_z = []
         
-        sampled_idx = tf.random.uniform([n], maxval=len(self), dtype=tf.int32)
-
-        for i in sampled_idx:
+        for i in self._rng.choice(len(self), size=n, replace=False):
             x.append(self[i][1])
             y_pi.append(self[i][2])
             y_z.append([self[i][3]])
             
-        return (tf.constant(x, dtype=tf.float64), 
-                [tf.constant(y_pi), tf.constant(y_z, dtype=tf.float64)])         
-    
+        return (np.array(x), [np.array(y_pi), np.array(y_z)])  
+        
