3e25b672914dfd2a54ae8784dc8b9deb177f2f06
Hahyun Baek <hahyun8587@gmail.com>
2024-04-10 20:58:25 +0900

    feat: support dense reward in train()

--- alphazero/agent.py
+++ alphazero/agent.py
@@ -19,12 +19,12 @@ class Agent():
         Node.configure(model, simulator, type)
 
     
-    def train(self, steps: int=700000, epochs: int=1,
-              alpha: float=1.0, epsilon: float=0.25,
-              episodes: int=100, replay_episodes: int=50000,
-              simulations: int=800, 
-              total_batch_size: int=4096, mini_batch_size: int=32, 
-              save_steps: int=1000, save_path: str='bin/alphazero.tf') -> None:
+    def train(self, steps: int = 700000, episodes: int = 100, 
+              simulations: int = 800, replay_buffer_size: int = 50000,
+              epochs: int = 1, examples: int = 4096, mini_batch_size: int = 32, 
+              alpha: float = 1.0, epsilon: float = 0.25, 
+              save_steps: int = 1000, 
+              save_path: str = "bin/alphazero.keras") -> None:
         """Trains model of agent `epochs` epochs for `steps` steps. 
         The agent self-plays `episodes` episodes using mcts and trains 
         using generated episode data.
@@ -37,8 +37,19 @@ class Agent():
         Args: 
             steps (int, optional): The number of steps to train. 
                 Setted to `700000` by default.
+            episodes (int, opitional): The number of episodes 
+                that the agent progresses before training in one step. 
+                Setted to `100` by default.
+            simulations (int, optional): The number of mcts simulations 
+                conducted. Setted to `800` by default.
+            replay_buffer_size (int, optional): The number of episodes to be 
+                saved in replay buffer. Setted to `50000` by default.
             epochs (int, optional): The number of epochs to train in one step.
                 Setted to `1` by default.
+            examples (int, optional): The number of data sampled from the 
+                replay buffer for training. Setted to `4096` by default.
+            mini_batch_size (int, optional): Mini batch size of data 
+                for training. Setted to `32` by default.
             alpha (float, optional): Dirichlet noise parameter. 
                 If alpha is smaller than `1.0`, the distribution vectors 
                 become near the basis vector. If alpha is `1.0`, 
@@ -49,17 +60,6 @@ class Agent():
                 legal actions. Setted to `1.0` by default.
             epsilon (float, optional): Weight of dirichlet noise. 
                 Setted to `0.25` by default.  
-            episodes (int, opitional): The number of episodes 
-                that the agent progresses before training in one step. 
-                Setted to `100` by default.
-            replay_episodes (int, optional): The number of episodes to be saved 
-                in replay memory. Setted to `50000` by default.
-            simulations (int, optional): The number of mcts simulations 
-                conducted. Setted to `800` by default.
-            total_batch_size (int, optional): Total batch size of data 
-                for training. Setted to `4096` by default.
-            mini_batch_size (int, optional): Mini batch size of data 
-                for training. Setted to `32` by default.
             save_steps (int, optional): The number of steps 
                 for saving the model. Setted to `1000` by default. 
             save_path (str, optional): File path for saving model. 
@@ -68,54 +68,48 @@ class Agent():
         
         model = Node.get_model()
         simulator = Node.get_simulator()
-        replay_mem = ReplayMemory(replay_episodes)
+        replay_mem = ReplayMemory(replay_buffer_size)
         rng = np.random.default_rng()
+        gamma = Node.get_gamma()
         type = Node.get_type()
 
         for i in range(1, steps + 1):
             for j in range((i - 1) * episodes + 1, i * episodes + 1):
                 episode_buffer = []
+                episode_rewards = []
                 
                 s = simulator.gen_init_s()
-                
                 p, v = model(s[np.newaxis, :], False)
-                
-                p = p.numpy().reshape(-1)
                 p = ((1 - epsilon) * p 
                      + epsilon * rng.dirichlet([alpha for _ in range(p.shape[0])]))
                 
-                v = v.numpy().item()
-                
-                root = Node(s, v, p, False)
+                root = Node(s, p, False)
 
                 while 1:
                     pi = root.mcts(simulations)
+                    a = rng.choice(pi.shape[0], p=pi)
                     
                     episode_buffer.append([j, root.get_s(), pi])
+                    episode_rewards.append(root.get_reward(a))
                     
-                    root = root.get_child(rng.choice(pi.shape[0], p=pi))
+                    root = root.get_child(a)
                     
                     if root.get_is_terminal():
                         break
                 
-                z = root.get_v_s()
+                traj_len = len(episode_buffer)
                 
-                if type == 0:
-                    for data in episode_buffer:
-                        data.append(z)
-                else:
-                    if len(episode_buffer) % 2 != 0:
-                        z = -z
-                    
-                    for data in episode_buffer:
-                        data.append(z)
-                        z = -z
+                episode_buffer[traj_len - 1].append(episode_rewards[traj_len - 1])
                 
+                for k in range(traj_len - 2, -1, -1):
+                    v = episode_buffer[k + 1][3] if type == 0 \
+                            else -episode_buffer[k + 1][3]
+                    episode_buffer[k].append(episode_rewards[k] + gamma * v)
+        
                 replay_mem.extend(episode_buffer)
 
-            for _ in range(epochs):
-                x, y = replay_mem.sample(total_batch_size)
-                model.fit(x, y, batch_size=mini_batch_size)
+            x, y = replay_mem.sample(examples)
+            model.fit(x, y, epochs=epochs, batch_size=mini_batch_size)
             
             if i % save_steps == 0:
                 model.save(save_path)    
